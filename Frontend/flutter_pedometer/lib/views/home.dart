97f26fe75ba6ff08eb4e714fc0bbaa98a260d557
AverageMalchik <77982377+AverageMalchik@users.noreply.github.com>
2022-12-11 21:34:16 +0530

    feat: Add HomePage

--- Frontend/flutter_pedometer/lib/views/home.dart
+++ Frontend/flutter_pedometer/lib/views/home.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pedometer/constants/txt_decoration.dart';
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeState extends State<Home> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Container(
-            width: 100,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.amber,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Steps",
+                style: TxtDecoration.textTheme.headline3,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                "99",
+                style: TxtDecoration.textTheme.headline2,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Text(
+                "Pedestrian status:",
+                style: TxtDecoration.textTheme.bodyText1,
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                "(walking)",
+                style: TxtDecoration.textTheme.bodyText2,
+              )
+            ],
           ),
         ),
       ),
